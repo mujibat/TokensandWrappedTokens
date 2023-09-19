ded0fbf9c7231b3b4e33b74584dadad93ec8fbcb
mujibat <oshikoyadolapo13@gmail.com>
2023-09-19 23:24:49 +0100

    d

--- contracts/LMAOT.sol
+++ contracts/LMAOT.sol
@@ -22,14 +22,31 @@ contract LMAOToken is ERC20 {
         return _chargesAccount[_lmaotoken];
     }
     
-    function transfer(address _to, uint256 _tokenamount) public virtual override returns (bool) {
-          uint deductLMAOToken = _tokenamount * 8 / 100;
-          uint getLMAOToken = _tokenamount - deductLMAOToken;
-        address owner = msg.sender;
-        _transfer(owner, _to, getLMAOToken);
-        _transfer(owner, deployer, deductLMAOToken);
-        return true;
-    }
+    // function transfer(address _to, uint256 _tokenamount) public virtual override returns (bool) {
+    //       uint deductLMAOToken = _tokenamount * 8 / 100;
+    //       uint getLMAOToken = _tokenamount - deductLMAOToken;
+    //     address owner = msg.sender;
+    //     _transfer(owner, _to, getLMAOToken);
+    //     _transfer(owner, deployer, deductLMAOToken);
+    //     return true;
+    // }
+    //   function transferFrom(address from, address to, uint256 amount) public virtual override returns (bool) {
+    //     address spender = _msgSender();
+    //     _spendAllowance(from, spender, amount);
+    //     uint deductLMAOToken = amount * 8 / 100;
+    //       uint getLMAOToken = amount - deductLMAOToken;
+    //     _transfer(from, to, getLMAOToken);
+    //     _transfer(from, deployer, deductLMAOToken);
+    //     return true;
+    // }
+     function _transfer(address from, address to, uint256 amount) internal virtual override {
+          uint deductLMAOToken = amount * 8 / 100;
+          uint getLMAOToken = amount - deductLMAOToken;
+          super._transfer(from, to, getLMAOToken);
+        super._transfer(from, deployer, deductLMAOToken);
+     }
+
+
 }
 
 
@@ -47,10 +64,12 @@ contract WrappedLMAOToken is ERC20 {
     mapping(address=> uint) balances;
    
 
-    function depositLMAOtoWLMAO(uint _LMAOamount, address _to) external {
+    function depositLMAOtoWLMAO(uint _LMAOamount, address _to) external payable  {
          require(_LMAOamount > 0, "Amount to deposit must be greater than 0");
-        uint deductLMAOTokenFee = _LMAOamount * 8 / 100;
+          uint deductLMAOTokenFee = _LMAOamount * 8 / 100;
         uint getWLMAOToken = _LMAOamount - deductLMAOTokenFee;
+        
+         ILMAO(lmaotoken).transfer(owner, _LMAOamount);
         _mint(_to, getWLMAOToken);
         balances[_to] += getWLMAOToken;
 
